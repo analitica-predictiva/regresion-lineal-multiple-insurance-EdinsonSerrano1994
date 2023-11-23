4d3a271138584f8dee0ad297e4eb3bdd435bb750
EdinsonSerrano1994 <126212940+EdinsonSerrano1994@users.noreply.github.com>
2023-11-23 14:22:07 -0500

    init

--- preguntas.py
+++ preguntas.py
@@ -88,7 +88,7 @@ def pregunta_03():
             # las variables.
             (
                 "column_transfomer",
-                make_column_selector(
+                make_column_transformer(
                     (
                         OneHotEncoder(),
                         make_column_selector(dtype_include=object),
