b49709eeb235868c2b8225c840fc385e23e0d352
EdinsonSerrano1994 <126212940+EdinsonSerrano1994@users.noreply.github.com>
2023-11-23 14:20:32 -0500

    init

--- preguntas.py
+++ preguntas.py
@@ -10,7 +10,7 @@ selección de las n variables más relevantes usando una prueba f.
 # pylint: disable=unsubscriptable-object
 
 import pandas as pd
-import numpy as np
+
 
 def pregunta_01():
     """
@@ -115,7 +115,7 @@ def pregunta_03():
 
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
-    param_grid = {'selectKbest_k': np.arange(1,12,1),
+    param_grid = {'selectKbest_k': range(1,12),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
